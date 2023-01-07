cbf31c95465379c9a23cd924e1554ec6e41f4c09
supra <68041302+suwupra@users.noreply.github.com>
2023-01-06 19:14:28 -0700

    Update PerformanceMode.lua

--- PerformanceMode.lua
+++ PerformanceMode.lua
@@ -1,1 +1,9 @@
-game.Players.LocalPlayer:Kick('not ready yet')
+for _, part in pairs(workspace:GetDescendants()) do
+		if part:IsA("BasePart") then
+			part.Material = Enum.Material.Plastic
+      part.Transparency = 1
+		end
+	end
+ RunService:Set3dRenderingEnabled(true)
+
+while task.wait() do setfpscap(5) end
